9e4ececf32abb76612605d94612be09d98c59945
Uxsx <snowlevelup@gmail.com>
2025-07-12 01:19:30 +0700

    home work 3

---
+++ Lab1/lab1_hw3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    char Name[50];
+    char LastName[50];
+
+    printf("Enter your name: ");
+    scanf("%s", Name);
+    printf("Enter your lastname: ");
+    scanf("%s", LastName);
+
+    printf("%s %s TC, RMUTL, Chiang Mai, Thailand\n", Name, LastName);
+    return 0;
+}
