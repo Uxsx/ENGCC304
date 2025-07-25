e42086128d43e7457200b121ce931b8dd135827b
Uxsx <snowlevelup@gmail.com>
2025-07-25 23:17:17 +0700

    add lab4

---
+++ Lab4/lab4.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    char EmpId[11]; 
+    int hrs;
+    float rate, salary;
+
+    printf("Input the Employees ID(Max. 10 chars): ");
+    scanf("%s", EmpId);
+
+    printf("Input the working hrs: ");
+    scanf("%d", &hrs);
+
+    printf("Salary amount/hr: ");
+    scanf("%f", &rate);
+
+    salary = hrs * rate;
+
+    printf("- - - - - - - - - - - - - \n") ;
+    printf("Employee ID = %s\n", EmpId);
+    printf("Salary = U$ %.2f\n", salary);
+
+    return 0;
+}
