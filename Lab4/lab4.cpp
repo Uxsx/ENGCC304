df7a4d8d1112c75e97b49dd12d35105028ff34ea
Uxsx <snowlevelup@gmail.com>
2025-07-26 23:24:04 +0700

    edit lab4

--- Lab4/lab4.cpp
+++ Lab4/lab4.cpp
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    char EmpId[11]; 
-    int hrs;
-    float rate, salary;
+    char EmpId[11] ; 
+    int hrs ;
+    float rate, salary ;
 
-    printf("Input the Employees ID(Max. 10 chars): ");
-    scanf("%s", EmpId);
+    printf("Input the Employees ID(Max. 10 chars): ")  ;
+    scanf("%s", EmpId) ;
 
-    printf("Input the working hrs: ");
-    scanf("%d", &hrs);
+    printf("Input the working hrs: ") ;
+    scanf("%d", &hrs) ;
 
-    printf("Salary amount/hr: ");
-    scanf("%f", &rate);
+    printf("Salary amount/hr: ") ;
+    scanf("%f", &rate) ;
 
     salary = hrs * rate;
 
     printf("- - - - - - - - - - - - - \n") ;
-    printf("Employee ID = %s\n", EmpId);
-    printf("Salary = U$ %.2f\n", salary);
+    printf("Employee ID = %s\n", EmpId) ;
+    printf("Salary = U$ %.2f\n", salary) ;
 
-    return 0;
+    return 0 ;
 }
