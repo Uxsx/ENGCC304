647b4981e73d12156e3206cee70d5afc7e838350
Uxsx <snowlevelup@gmail.com>
2025-10-06 23:13:28 +0700

    edit lab7

--- Lab7/Lab07-While-DoWhile.cpp
+++ Lab7/Lab07-While-DoWhile.cpp
@@ -3,44 +3,63 @@
 #include <time.h>
 
 int main() {
-    int select;
-    srand(time(NULL));   
-    do {
-        printf("Do you want to play game (1=play,-1=exit) :\n");
-        scanf("%d", &select);
+    int choice ;
+    int guess ;
+    int answer ;
+    int score ;
 
-        if (select == 1) {
-            int number = rand() % 100 + 1;  
-            int guess, score = 100;
-            int low = 1, high = 100;       
+    srand(time(NULL)) ; 
 
-            printf("\n(Score=%d)\n\n", score);
-
-            while (1) {
-                printf("Guess the winning number (%d-%d) :\n", low, high);
-                scanf("%d", &guess);
-
-                if (guess == number) {
-                    printf("\nThat is correct! The winning number is %d.\n", number);
-                    printf("\nScore this game: %d\n\n", score);
-                    break;
-                } else {
-                    score -= 10; 
-
-                    if (guess < number) {
-                        printf("\nSorry, the winning number is HIGHER than %d. (Score=%d)\n\n", guess, score);
-                        if (guess + 1 > low) low = guess + 1;
-                    } else {
-                        printf("\nSorry, the winning number is LOWER than %d. (Score=%d)\n\n", guess, score);
-                        if (guess - 1 < high) high = guess - 1;
-                    }
-                }
-            }
-        } else if (select == -1) {
-            printf("\nSee you again.\n");
+    while (1) {
+        printf("Do you want to play game (1=play,-1=exit) : ") ;
+        if (scanf("%d", &choice) != 1) {
+            printf("Please enter only 1 or -1.\n\n") ;
+            while (getchar() != '\n') ;
+            continue ;
         }
 
-    } while (select != -1);
+        if (choice == -1) {
+            printf("\nSee you again.\n") ;
+            break ;
+        } else if (choice != 1) {
+            printf("Please enter only 1 or -1.\n\n") ;
+            continue ;
+        }
 
-    return 0;
+        // Restart
+        answer = rand() % 100 + 1 ;
+        score = 100 ;
+
+        printf("\n(Score=%d)\n", score) ;
+
+        int low = 1, high = 100 ; 
+
+        while (1) {
+            printf("\nGuess the winning number (%d-%d) : ", low, high) ;
+            scanf("%d", &guess) ;
+
+            if (guess == answer) {
+                printf("\nThat is correct! The winning number is %d.\n", answer) ;
+                printf("\nScore this game: %d\n\n", score) ;
+                break ;
+            } else {
+                score -= 10 ;
+                if (score <= 0) {
+                    printf("\nYou lose! The winning number was %d.\n", answer) ;
+                    printf("Score this game: 0\n\n") ;
+                    break ;
+                }
+
+                if (guess < answer) {
+                    printf("\nSorry, the winning number is HIGHER than %d. (Score=%d)\n", guess, score) ;
+                    if (guess + 1 > low) low = guess + 1 ;
+                } else {
+                    printf("\nSorry, the winning number is LOWER than %d. (Score=%d)\n", guess, score) ;
+                    if (guess - 1 < high) high = guess - 1 ;
+                }
+            }
+        }
+    }
+
+    return 0 ;
 }
